8ae19fb34ae9d634381a1116dddace602d31bb1e
Juncheol Cho <zironycho@gmail.com>
2017-02-10 01:21:20 +0900

    change the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='account-book',
-    version='0.2',
+    version='0.2.0',
     py_modules=find_packages(),
     install_requires=[
         'pyyaml',
