4b68cfffa1b8a5186632a45c35ecd4ca9e543ed0
Juncheol Cho <zironycho@gmail.com>
2021-01-31 23:25:30 +0900

    add 롯데카드

--- accountbook/money.py
+++ accountbook/money.py
@@ -34,6 +34,8 @@ def _get_money_part(text):
             money_part = text.split(')')[1]
         elif '씨티카드' in text:
             money_part = text.split('일시불')[1]
+        elif '롯데' in text:
+            money_part = text.split(' ')[4]
 
     money = re.findall(r'\d[\d,\.]*', money_part)[0]
     # print(money)
